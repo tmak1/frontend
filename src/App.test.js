239442292298650f64cac6248f9da995be478382
tmak1 <tarik.khan.git@gmail.com>
2025-04-06 15:43:57 +1000

    w

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from "./App";
 
 it("renders learn react link", () => {
   render(<App />);
-
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
